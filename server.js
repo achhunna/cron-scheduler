55c7b3223376a6497623cb1d281cee5a4e8444de
achhunna <achhunna@gmail.com>
2018-04-28 16:19:13 -0700

    dynamic port assignment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Express server
 const express = require('express');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
