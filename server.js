cc996c63968bde074902635e587697649c23eaeb
achhunna <achhunna@gmail.com>
2018-04-28 16:23:34 -0700

    fix public path

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 app.listen(port);
 console.log('listening on port', port);
